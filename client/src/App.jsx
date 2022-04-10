e286ae0f0d5e47c104b7f8a41b9aa9b432a645d2
Nikita Azarov <n.azarov@coretechs.ru>
2022-04-10 14:51:21 +0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyleContextProvider } from './contexts';
+
+const App = () => {
+  return <StyleContextProvider>content</StyleContextProvider>;
+};
+
+export default App;
